e3c6f36206d2e2e4629a210cd5d1a2bb05278908
heliumwyrm <heliumwyrm@gmail.com>
2021-06-11 01:19:05 +0800

    Add the bare minimum of packaging stuff

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mkultra',
+      version='0.1',
+      description='Prompt tuning toolkit for GPT-2 and GPT-Neo',
+      url='https://github.com/corolla-johnson/mkultra',
+      author='Corolla Johnson',
+      author_email='no_email_given@gmail.com',
+      license='MIT',
+      packages=['mkultra'])
