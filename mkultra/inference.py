12fb74886bae8d48d16c8752480740d7b197095a
heliumwyrm <heliumwyrm@gmail.com>
2021-06-20 20:29:46 +0800

    Add batch generation support

--- mkultra/inference.py
+++ mkultra/inference.py
@@ -16,19 +16,27 @@ class GPTSoftPromptMixin:
         # Embed everything normally first
         inputs_embeds = self.transformer.wte(input_ids.to(self.device))
 
-        # Replace special tokens with soft prompts
-        for i in range(input_ids.shape[-1]):
-            # Check each id for a special token
-            input_id = input_ids[0,i].item()
-            sp = SoftPrompt.from_input_id(input_id)
-            # If we find one, replace special token and padding with soft prompt
-            if sp:
-                replacement = sp.get_inputs_embeds().to(self.device).clone().unsqueeze(0)
-                inputs_embeds[0,i:i+len(sp),:] = replacement[0,:,:]
+        n_batches = input_ids.shape[0]
+        n_tokens = input_ids.shape[-1]
+
+        for b in range(n_batches):
+            # Replace special tokens with soft prompts
+            for t in range(n_tokens):
+                # Check each id for a special token
+                input_id = input_ids[b,t].item()
+                sp = SoftPrompt.from_input_id(input_id)
+                # If we find one, replace special token and padding with soft prompt
+                if sp:
+                    replacement = sp.get_inputs_embeds().to(self.device).clone().unsqueeze(0)
+                    inputs_embeds[b,t:t+len(sp),:] = replacement[0,:,:]
 
         return inputs_embeds
 
     def forward(self, *args, **kwargs):
+        # Alow setting input_ids as positional arg 0
+        if kwargs.get('input_ids') is None:
+            kwargs['input_ids'] = args[0]
+
         input_ids = kwargs.get('input_ids').to(self.device)
 
         if input_ids is None:
@@ -38,10 +46,16 @@ class GPTSoftPromptMixin:
         kwargs['input_ids'] = None
         kwargs['inputs_embeds'] = self.replace_special_tokens(input_ids)
 
+        args = ()
+
         return super().forward(*args, **kwargs)
 
     @torch.no_grad()
     def generate(self, *args, **kwargs):
+        # Alow setting input_ids as positional arg 0
+        if kwargs.get('input_ids') is None:
+            kwargs['input_ids'] = args[0]
+
         # This fixes CUDA for some reason
         kwargs['input_ids'] = kwargs['input_ids'].to(self.device)
 
@@ -51,6 +65,8 @@ class GPTSoftPromptMixin:
         for id in SoftPrompt.get_special_token_ids():
             bad_words_ids.append([id])
         kwargs['bad_words_ids'] = bad_words_ids
+
+        args = ()
 
         return super().generate(*args, **kwargs)
 
